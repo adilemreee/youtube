842d548e7f38b6832e895d5fc8872a303812506f
Adil Emre <adil.emre@hotmail.com>
2025-12-25 00:51:50 +0300

    Initial Commit

---
+++ youtube/youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeApp.swift
+//  youtube
+//
+//  Created by adil emre  on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
